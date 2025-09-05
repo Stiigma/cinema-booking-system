b3d48c387c1b2d7794c3d05577694f69da9bee5e
Stiigma <eduardoby14@gmail.com>
2025-09-05 10:45:02 -0700

    Inicio de proyecto

---
+++ movie-reservation/src/test/java/com/moviebkgsys/movie_reservation/MovieReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviebkgsys.movie_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
